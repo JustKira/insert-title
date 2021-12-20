4472ecc6302cd55ee307bb3374783d71a84c3ef3
JustKira <84239732+JustKira@users.noreply.github.com>
2021-12-20 21:48:31 +0200

    small changes recruiter_interface

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('api/',include('ral.api.urls')),
     path('api/',include('student_interface.api.urls')),
+    path('api/',include('recruiter_interface.api.urls')),
 ]
--- backend/recruiter_interface/models.py
+++ backend/recruiter_interface/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Recruiter(models.Model):
-    
+
+    user = models.ForeignKey(User,on_delete=CASCADE,null=True)
     first_name = models.TextField()
     middle_name = models.TextField()
     last_name = models.TextField()
@@ -13,17 +15,17 @@ class Recruiter(models.Model):
     birthdate = models.DateTimeField()
     bio = models.TextField()
 
-    contact_links = models.TextField()
+    contact_links = models.TextField(null=True,blank=True)
 
-    is_company = models.BooleanField()
+    is_company = models.BooleanField(null=True,blank=True)
 
     # If is_company == True
-    institution_name = models.TextField()
-    company_size = models.TextField()
-    industry = models.TextField()
-    company_description = models.TextField()
+    institution_name = models.TextField(null=True,blank=True)
+    company_size = models.TextField(null=True,blank=True)
+    industry = models.TextField(null=True,blank=True)
+    company_description = models.TextField(null=True,blank=True)
 
     # IMPORTANT
-    verified = models.BooleanField()
+    verified = models.BooleanField(null=True,blank=True)
 
     careers = models.ManyToManyField('roadmap_system.Career')
