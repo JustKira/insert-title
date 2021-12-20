4404713f3e607439945e9484bcde8ccc9d60fdd7
yradwan147 <54478004+yradwan147@users.noreply.github.com>
2021-12-20 17:47:45 +0200

    Added homepage animation and added dynamic textbox (going to change background_image ASAP)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,12 +10,12 @@ const Header = () => {
         <div className="header">
             <ul className='nav'>
                 <li id='headerlogo'><Link to="/"><img id='header_logo' src="assets/logo.png"/></Link></li>
-                <li>{user && <p id='greeting'>Hello {user.user_id}</p>}</li>
+                <li>{user && <a id='greeting'>Hello {user.username}</a>}</li>
                 <li><Link to="/postings">Postings</Link></li>
                 <li><Link to="/profile/">Profile</Link></li>
                 <li><Link to="/career">Career</Link></li>
                 {user ? (
-                <li><p onClick={logoutUser}>Logout</p></li>
+                <li><a onClick={logoutUser}>Logout</a></li>
             ):(
                 <li><Link to="/Login"> Login</Link></li>
                 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,12 +3,30 @@ import DescriptionBlock from '../components/DescriptionBlock'
 import DescriptionBlockRev from '../components/DescriptionBlockRev'
 
 const HomePage = () => {
+    let names = ['Doctor', 'Engineer', 'Programmer', 'YOU']
+    let landing_animation = ()=>{
+        console.log('here')
+        setInterval(async ()=>{
+        const e = document.getElementById('landing_text')
+        names.forEach(async (name, index)=>{
+            setTimeout(()=>{e.value = name}, index*1000)
+        })
+    },10000);
+    }
+    landing_animation()
+
     return (
         <div className='content'>
             <div className='landing'>
                 <div className='center'>
-                    <input className='landing_text' type="text"/>
+                    <div className='landing_textblock'>
+                        <label className='landing_textlabel'>INSERT</label>
+                        <input id='landing_text' type="text"/>
+                    </div>
                 </div>
+            </div>
+            <div className='landing_slogan'>
+                <h2>"Committed to making our ever-evolving job market ever-easier to enter"</h2>
             </div>
             <div className='landing_desc_wrapper'>
                 <DescriptionBlock text='Just insert your title above, and get a collection of internships fitting your criteria and filtered for undergraduates' img='desc1.png' header='Tired of looking for internships'/>
